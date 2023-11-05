d8e67783e0ec967217a0543f975e2043f008247d
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2023-11-04 21:33:49 -0500

    delete redundant comments

--- bat.sh
+++ bat.sh
@@ -12,20 +12,15 @@ BAT=$(pmset -g batt | awk -F'[\t;%]+' 'NR==2{print $2}')
 
 echo "your current battery is $BAT%"
 
-#if (( BAT <= BATTHL ))
 if [ "$BAT" -le "$BATTHL" ]
 then
     #echo -en "\007"  
     echo "low battery"
-    #osascript -e 'display notification "Low battery" with title "Battery Alert"'
-    #osascript -e 'set volume 0'
-    #osascript -e 'display dialog "Connect the charger 🥹" with title "Low Battery Alert"'
     osascript -e "display dialog \"Connect the charger 🥹 Your battery is at $BAT%\" with title \"Low Battery Alert\""
     
 elif [ "$BAT" -ge "$BATTHH" ]
 then
     echo "High battery"
-    #osascript -e 'display dialog "Disconnect the charger 🤯" with title "High Battery Alert"'
     osascript -e "display dialog \"Disconnect the charger 🤯 Your battery is at $BAT%\" with title \"High Battery Alert\""
 fi
     sleep 600
