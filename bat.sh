96093cf80ddae33e34f1b8184e8a83c8c6c2b5c1
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2023-11-17 10:14:30 -0600

    avoid redundant dialog box

--- bat.sh
+++ bat.sh
@@ -2,26 +2,35 @@
 
 echo "Hello, $(whoami)!"
 
-BATTHL="23"
-BATTHH="77"
+BATTHL="83"
+BATTHH="84"
+BATTERYMODE="Battery"
+ACMODE="AC"
 
 while true
 do 
 BAT=$(pmset -g batt | awk -F'[\t;%]+' 'NR==2{print $2}')
 #BAT=$(pmset -g batt | grep -o '[0-9]*%' | tr -d '%')
+CHARGEMODE=$(pmset -g batt | awk -F'[\t \047]+' 'NR==1{print $4}')
 
 echo "your current battery is $BAT%"
 
 if [ "$BAT" -le "$BATTHL" ]
 then
-    #echo -en "\007"  
+    if [[ "$CHARGEMODE" == "$BATTERYMODE" ]]
+    then 
+        osascript -e "display dialog \"Connect the charger 🥹 Your battery is at $BAT%\" with title \"Low Battery Alert\""
+    fi 
     echo "low battery"
-    osascript -e "display dialog \"Connect the charger 🥹 Your battery is at $BAT%\" with title \"Low Battery Alert\""
     
 elif [ "$BAT" -ge "$BATTHH" ]
 then
+    if [[ "$CHARGEMODE" == "$ACMODE" ]]
+    then
+        osascript -e "display dialog \"Disconnect the charger 🤯 Your battery is at $BAT%\" with title \"High Battery Alert\""
+    fi
     echo "High battery"
-    osascript -e "display dialog \"Disconnect the charger 🤯 Your battery is at $BAT%\" with title \"High Battery Alert\""
+    
 fi
-    sleep 600
+    sleep 10
 done
